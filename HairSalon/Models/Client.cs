cf94dbdb21075330a4deaa23800370f3219e860a
sethgonzales <sethgonzales157@gmail.com>
2023-10-06 14:23:09 -0700

    add styling to create pages for client and stylists

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,9 +2,10 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public int ClientId { get; set; } 
+    public int ClientId { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
-    public Stylist Stylist { get; set; } 
-    public int StylistId { get; set; } 
+    public Stylist Stylist { get; set; }
+    public int StylistId { get; set; }
   }
 }
